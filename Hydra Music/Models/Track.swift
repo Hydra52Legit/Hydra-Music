0dcff8629f562a495f69855be485401ee0bdeb24
Hydra <hydrawork777@gmail.com>
2026-04-21 17:06:44 +0500

    create Track model

--- Hydra Music/Models/Track.swift
+++ Hydra Music/Models/Track.swift
@@ -32,4 +32,96 @@ enum RepeatMode: String, Codable {
 struct Track {
     var id: UUID
     var filaName: String
+    var title: String
+    var artist: String
+    var duration: TimeInterval
+    var atworkPAth: String?
+    var bookmarData: Data?
+    var dateAdded: Date
+    
+    init (
+        id: UUID = UUID(),
+        fileName: String,
+        title: String,
+        artist: String = "Неизвестный исполнитель",
+        duration: TimeInterval = 0,
+        artworkPath: String? = nil,
+        bookmarkData: Data? = nil,
+        dateAdded: Date = Date()
+    ){
+        self.id = id
+        self.fileName = fileName
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+        self.artworkPath = artworkPath
+        self.bookmarkData = bookmarkData
+        self.dateAdded = dateAdded
+    }
+}
+extension Track: Identifiable {}
+
+extension Track: Codable {}
+
+extension Track: FetchableRecord {
+    init(row: Row) throws {
+            id           = row["id"]
+            fileName     = row["fileName"]
+            title        = row["title"]
+            artist       = row["artist"]
+            duration     = row["duration"]
+            artworkPath  = row["artworkPath"]
+            bookmarkData = row["bookmarkData"]
+            dateAdded    = row["dateAdded"]
+        }
+}
+extension Track: PersistableRecord {
+    static var databaseTableName: String = { "tracks" }
+    
+    func encode(to container: inout PersistenseContainer) throws {
+            container["id"]           = id.uuidString
+            container["fileName"]     = fileName
+            container["title"]        = title
+            container["artist"]       = artist
+            container["duration"]     = duration
+            container["artworkPath"]  = artworkPath
+            container["bookmarkData"] = bookmarkData
+            container["dateAdded"]    = dateAdded
+    }
+}
+extension Track {
+    var formattedDuration: String {
+        let minutes = Int(duration) / 60
+        let seconds = Int(duration) % 60
+        return String(format: "%d:%02d", minutes, seconds)
+    }
+    
+    var fileURL: URL? {
+        guard let documentsURL = FileManager.default
+            .urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return documentsURL
+            .appendingPathComponent("AppMusic")
+            .appendingPathComponent(fileName)
+    }
+}
+extension Track {
+    func toDictionary(artwork: UIImage? = nil) -> [String: Any] {
+        var info: [String: Any] = [
+            MPMediaItemPropertyTitle:            title,
+            MPMediaItemPropertyArtist:           artist,
+            MPMediaItemPropertyPlaybackDuration: duration,
+            MPNowPlayingInfoPropertyElapsedPlaybackTime: 0,
+            MPNowPlayingInfoPropertyPlaybackRate: 1.0,
+        ]
+        
+        if let image = artwork {
+            
+            let artworkItem = MPMediaItemArtwork(boundsSize: image.size) { _ in image }
+            info[MPMediaItemPropertyArtwork] = artworkItem
+        }
+        
+        return info
+    }
 }
