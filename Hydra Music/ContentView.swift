6992bbddfdb5bf321129cdb17488922e3de8cc55
Hydra <hydrawork777@gmail.com>
2026-04-21 16:25:57 +0500

    Initial Commit

---
+++ Hydra Music/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hydra Music
+//
+//  Created by Alex Kornilov on 21. 4. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hydra Music/Hydra_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hydra_MusicApp.swift
+//  Hydra Music
+//
+//  Created by Alex Kornilov on 21. 4. 2026..
+//
+
+import SwiftUI
+
+@main
+struct Hydra_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
